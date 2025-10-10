616e907d8dc8298ceca448e24ba5bc0811ca87d9
Anonymous <bkdhaniswar7991@gmail.com>
2025-10-10 14:24:47 +0200

    function pointer

---
+++ Functions/function_pointer.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+
+/*
+Function Pointer
+
+syntax:
+
+return_type_of_fun (*pointer_namr)(int, int);
+
+int sum(int a, int b){
+
+return a + b;
+
+}
+
+
+int (*ptr)(int, int) = &sum;
+
+
+
+
+*/
+
+
+
+int sum(int a, int b){
+
+    return a + b;
+}
+
+
+
+
+void main() {
+
+    int s =0;
+
+    // int (*ptr)(int, int) = &sum;
+    int (*ptr)(int, int) = sum; // both are works
+
+
+    // s = (*ptr)(2,3);
+    s = ptr(2,3); // both work fine
+    printf("%d\n", s);
+
+
+    
+}
+
+
+
+
+
+
+
+
