f113bb823ead7daaf912b601231524a09ec1f67e
Anonymous <bkdhaniswar7991@gmail.com>
2025-10-11 13:21:56 +0200

    Singly Linked list

---
+++ Lineked_List/singly_linked_list.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/*
+1). Singly Linked list
+
+=> we can modify thes linked list while implementing thise according to our logic
+
+=> As the name suggest it is going to have single pointer
+
+=> In sinlgly linked list each node contains two part one is  data part next part is addredd part(a pointer to is next node)
+
+=> And the address of the first node is store in the pointer/head pointer/ start pointer
+
+=> And Address part of last node contain Null value
+
+=> Only forward traversal is possible in the list
+
+=> Only forward sequential movement is possible
+
+*/
+
+struct node{
+    int a;
+    struct node *next;
+};
+
+
+int main() {
+
+    return 0;
+}
---
+++ Lineked_List/types_of_linked_list.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/*
+There are basically 4 types of linked list
+1). Singly Linked list
+2). Doubly Linked list
+3). Circular Linked list
+4). Doubly Circular Linked list
+
+1). we can modify thes linked list while implementing thise according to our logic
+*/
+
+
+struct node{
+    int a;
+    struct node *next;
+};
+
+
+int main() {
+
+    return 0;
+}
