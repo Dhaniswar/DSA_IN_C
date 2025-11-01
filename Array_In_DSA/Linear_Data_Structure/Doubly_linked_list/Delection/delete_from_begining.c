49fe37a503d8d65ba29f9943c006f980c0e2dbbe
Anonymous <bkdhaniswar7991@gmail.com>
2025-11-01 16:51:31 +0100

    Re-structured the folder according to their family

---
+++ Array_In_DSA/Linear_Data_Structure/Doubly_linked_list/Delection/delete_from_begining.c
@@ -0,0 +1,136 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+/*
+Creating the Doubly linked list
+
+Delete a node from the beginning in the Doubly linlked list
+*/
+
+struct node {
+
+    struct node *previous;
+    int data;
+    struct node* next;
+
+};
+
+int count = 0;
+
+
+
+struct node *create_doubly_linked_list(struct node *head, struct node **tail){
+     struct node  *newnode=NULL;
+
+     char choice[10];
+
+     do{
+        newnode= (struct node*)malloc(sizeof(struct node));
+
+        if(newnode==NULL){
+            printf("Memory Allocation Failed!\n");
+            return head;
+        }
+
+        printf("Enter the data: \n");
+        scanf("%d", &newnode->data);
+        newnode->previous = NULL;
+        newnode->next = NULL;
+
+        if(head==NULL){
+            head=*tail=newnode;
+        }
+
+        else{
+            (*tail)->next = newnode;
+            newnode->previous = *tail;
+            *tail = newnode;
+        }
+        printf("Do you want to continue? (yes/no): ");
+        scanf("%s", choice);
+        count++;
+        
+     } while(strcmp(choice, "yes") == 0 || strcmp(choice, "Yes") == 0 );
+
+     return head;
+}
+
+
+
+
+void display_doubly_linked_list(struct node *head){
+    struct node *temp=NULL;
+
+    if(head==NULL) {
+        printf("Empty list!\n");
+        return;
+    }
+
+    temp= head;
+    while(temp !=0){
+        printf("Previous=%p, Data=%d, and Next=%p\n", temp->previous, temp->data, temp->next);
+        temp= temp->next;
+    }
+
+}
+
+
+
+// Delete a node from the beginning in the Doubly linlked list
+struct node *delete_node_from_beginning(struct node *head, struct node **tail){
+
+    struct node *temp;
+
+    if(head==NULL){
+        printf("Empty linked list, there is nothing to delete!\n");
+        return head;
+    }
+
+    else{
+        temp = head;
+        head = head->next;
+        head->previous = NULL;
+    }
+    free(temp);
+
+    printf("Node deleted successfully from the beginning!\n");
+    return head;
+
+}
+
+void free_list(struct node *head) {
+
+    struct node *temp;
+
+
+    while(head !=0){
+        temp=head;
+        head= head->next;
+        free(temp);
+    }
+
+}
+
+
+
+int main() {
+
+    struct node *head=NULL, *tail=NULL;
+
+    head = create_doubly_linked_list(head, &tail);
+
+    printf("Original linked list: \n");
+    display_doubly_linked_list(head);
+
+
+    head = delete_node_from_beginning(head, &tail);
+    printf("linked list after deleting from the beginning: \n");
+    display_doubly_linked_list(head);
+
+
+    free_list(head);
+
+    return 0;
+}
