d35c05bf2b6e7d3fa6617c691e6af97e6dd6e6a4
Anonymous <bkdhaniswar7991@gmail.com>
2025-11-08 11:15:32 +0100

    Inserting a node at the beginning of the doubly circular linked list

---
+++ DSA/Linear_Data_Structure/Doubly_circular_linked_list/Insertion/insertion_operations_in_doubly_circular_linked_list.c
@@ -0,0 +1,167 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+/*
+Creating Double circular linked list
+
+*/
+
+struct node {
+    struct node *previous;
+    int data;
+    struct node *next;
+};
+
+int count = 0;
+
+struct node *create_doubly_circular_linked_list(struct node *head, struct node **tail){
+    struct node *newnode, *temp;
+
+    char choice[10];
+
+    do{
+
+    newnode=(struct node*)malloc(sizeof(struct node));
+    if(newnode==NULL){
+        printf("Memory allocation failed!\n");
+        return head;
+    }
+
+    printf("Enter data: \n");
+    scanf("%d", &newnode->data);
+    newnode->previous = NULL;
+    newnode->next = NULL;
+
+    if(head==NULL){
+        head=*tail=newnode;
+        newnode->next = newnode->previous = newnode;
+    }
+
+    else{
+        (*tail)->next = newnode;
+        newnode->previous = *tail;
+        newnode->next = head;
+        head->previous = newnode;
+        *tail = newnode;
+    }
+
+    
+    printf("Do you want to continue? (yes/no): ");
+    scanf("%s", choice);
+    count++;
+
+    }while(strcmp(choice, "yes") == 0 || strcmp(choice, "Yes") == 0);
+
+    return head;
+
+}
+
+
+struct node *insert_at_the_beginning(struct node *head, struct node **tail){
+    struct node *newnode;
+
+    newnode=(struct node*)malloc(sizeof(struct node));
+    if(newnode==NULL){
+        printf("Memory allocation failed!\n");
+        return head;
+    }
+
+    printf("Insert data at the beginning: \n");
+    scanf("%d", &newnode->data);
+    newnode->previous = NULL;
+    newnode->next = NULL;
+
+    if(head==NULL){
+        head=*tail=newnode;
+        newnode->next = newnode->previous = newnode;
+    }
+
+    else{
+        head->previous->next = newnode;
+        newnode->previous = head->previous;
+        newnode->next = head;
+        head->previous = newnode;
+        head = newnode;
+
+    }
+
+  
+    return head;
+
+}
+
+
+
+
+void display_doubly_circular_linked_list(struct node *head, struct node **tail){
+    struct node *temp;
+
+    temp = head;
+
+    if(head==NULL){
+        printf("Emplty list!\n");
+        return;
+    }
+
+    else{
+
+        do{
+            printf("Previous=%p, Data=%d, and Next=%p\n", temp->previous, temp->data, temp->next);
+            temp= temp->next;
+        }
+        while(temp != head); //even we can replace head with (*tail)->next
+        printf("Verify circular linked list Data=%d\n",(*tail)->next->data);
+        printf("Verify circular linked list Data=%d\n",head->previous->data);
+    }
+
+}
+
+
+
+void free_list(struct node *head, struct node **tail) {
+
+    if(head==NULL){
+        return;
+    }
+
+    struct node *temp, *current;
+
+    current = head;
+
+    do{
+        temp=current;
+        current=current->next;
+        free(temp);
+    }while(current !=head);
+
+    *tail = NULL;
+
+}
+
+
+
+
+
+
+int main(){
+
+    struct node *head=NULL, *tail=NULL;
+
+    head = create_doubly_circular_linked_list(head, &tail);
+
+    printf("Original linked list: \n");
+    display_doubly_circular_linked_list(head, &tail);
+
+    head = insert_at_the_beginning(head, &tail);
+
+    printf("Linked list after inserting in the beginning: \n");
+    display_doubly_circular_linked_list(head, &tail);
+
+    
+    free_list(head, &tail);
+
+
+
+    return 0;
+}
