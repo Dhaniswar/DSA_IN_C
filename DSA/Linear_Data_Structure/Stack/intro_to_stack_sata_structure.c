cf82fb099d5661d6d069bfa77cbd9dd01b3abaea
Anonymous <bkdhaniswar7991@gmail.com>
2025-11-08 17:06:22 +0100

    Introduction to stack data structure and algorithm

---
+++ DSA/Linear_Data_Structure/Stack/intro_to_stack_sata_structure.c
@@ -0,0 +1,17 @@
+/*
+Stack Data structure:
+=> It is linear data structure
+=> It a collection of similar data types
+=> It stack only limited access is possible
+=> It is an ordered list or collection or containner which is going to follow a rule for insertion and deletion of the data
+
+#rules 
+1). Insertion and deletion is possible only from one end.
+2). last in first out(LIFO) OR first in last out(FILO)
+
+#Operations and their technical name
+i). when you insert data in the stack is known as Push. Push=> Insertion operation
+ii). when you delere/remove data from the stack is known as Pop. Pop=> Deletion operation
+
+
+*/
