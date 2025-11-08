49ed999a474b11175d52a6b69c1dcde349f898e0
Anonymous <bkdhaniswar7991@gmail.com>
2025-11-08 19:12:38 +0100

    Understanding of Stack data structure and algorithm like it's working and it's application

--- DSA/Linear_Data_Structure/Stack/intro_to_stack_sata_structure.c
+++ DSA/Linear_Data_Structure/Stack/intro_to_stack_sata_structure.c
@@ -4,14 +4,35 @@ Stack Data structure:
 => It a collection of similar data types
 => It stack only limited access is possible
 => It is an ordered list or collection or containner which is going to follow a rule for insertion and deletion of the data
-
+ 
 #rules 
 1). Insertion and deletion is possible only from one end.
 2). last in first out(LIFO) OR first in last out(FILO)
 
 #Operations and their technical name
-i). when you insert data in the stack is known as Push. Push=> Insertion operation
-ii). when you delere/remove data from the stack is known as Pop. Pop=> Deletion operation
+i). when you insert data in the stack is known as Push. Push()=> Insertion operation
+ii). when you delere/remove data from the stack is known as Pop. Pop()=> Deletion operation
+
+and Push(x) => where x is the element or data for inserting
+and Pop() => Pop() will not take any parameter it means Pop out top most data from the stack
+and Peak()/top() => it is going to return the top most element from the stack with out removing that element from the stack
+and isEmpty()
+and isFull()
+and search(), minimunElemnet(), maximumElement(), traverseStack()
+
+#There are two ways to implement stack
+a). Using static memory allocation(that is using Array)
+b). Dynamic memory allocation(that means using linked list)
+
+
+#APPLICATION OF STACK
+a. To reverse a string or words (using stack is  very easy)
+b. Undo Mechanism (that means ctrl+z)
+c. Recursion/ function call
+d. To check the balance of parenthesis {} => the compiler used stack to verify the balance of the parenthesis
+e. Conversion of In fixed to Post fixed/pre fixed
+or many more like Topological sorting, DFL, tower of hanoi algorithm, tree traversal
+f. Depth-first search, evaluation of post fixed operation and many more
 
 
 */
