38c8668df6b88d90f79e8759bb2e78cbd4f3f0be
Anonymous <bkdhaniswar7991@gmail.com>
2025-11-10 16:47:20 +0100

    implemented Stack using linked list that is Dynamic memory allocation

--- DSA/Linear_Data_Structure/Stack/Using_linked_list/Imple_of_stack.c
+++ DSA/Linear_Data_Structure/Stack/Using_linked_list/Imple_of_stack.c
@@ -1,9 +1,87 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+
 
 
 
 /*
 Implementation of Stack, using Dynamic memory allocation(that is Linked list)
+1.) Whenever we are implementing stack using linked list then we always follow or do insrtion/deletion fron the beginnig(head) of the linked list
+2.) While implementing stack using linked list we will consider head as top
+3.) At the starting head/top value should be NULL
+4.) And first node address part should contail NULL value and need to update top value with that inserted node address
+5.) If we insert another node then unlike regular linked list that newnode address part contains address of previous node
+(but in regular linked list, previous node address part store the address on newnode)
 
 */
+
+struct stack {
+    int data;
+    struct stack *next;
+
+};
+
+
+struct stack *push(struct stack *top){
+    struct stack *newnode;
+    char choice[30];
+
+    do{
+
+    newnode=(struct stack*)malloc(sizeof(struct stack));
+
+    if(newnode == NULL){
+        printf("Memory allocation failed!");
+        return top;
+    }
+
+    printf("Enter the data to push in the stack: \n");
+    scanf("%d", &newnode->data);
+    newnode->next = NULL;
+
+    if(top == NULL){
+        top=newnode;
+    }
+
+    else{
+        newnode->next = top;
+        top = newnode;
+    }
+
+
+    printf("Do you want to continue: (yes, Yes, no, No):\n");
+    scanf("%s", choice);
+
+
+    }while(strcmp(choice, "yes")==0 || strcmp(choice, "Yes")==0);
+
+    return top;
+
+    
+
+}
+
+
+
+
+
+int main() {
+
+    struct stack *top=NULL;
+
+    top=push(top);
+
+    printf("Displaying stack data: \n");
+    display(top);
+
+    peek(top);
+
+
+
+    return 0;
+
+}
+
+
+
