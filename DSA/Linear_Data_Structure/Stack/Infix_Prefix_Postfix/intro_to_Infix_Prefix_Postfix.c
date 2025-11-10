5eae559eecb8b6b549a2d501f29ccd66f82189bf
Anonymous <bkdhaniswar7991@gmail.com>
2025-11-10 19:01:18 +0100

    list out the main presedence and associvity of Brackets and some operators

--- DSA/Linear_Data_Structure/Stack/Infix_Prefix_Postfix/intro_to_Infix_Prefix_Postfix.c
+++ DSA/Linear_Data_Structure/Stack/Infix_Prefix_Postfix/intro_to_Infix_Prefix_Postfix.c
@@ -3,4 +3,25 @@
 #include<string.h>
 
 /*
+Expression:
+Basically expression means it contains some constant, may be variable, or operands or operators or symbols
+eg(p+q, 4 - 2, a + 6)
+general form of biary expression is => <Operand> <Operator> <Operand> => and this form is known as Infix expression
+
+#Precedence of expression and Associvity
+1.) () {} []  L-R
+2.) ^         R-L
+3.) * /       L-R
+4.) + -       L-R
+
+Q1) 1 + 2 * 5 + 30/5
+=> 1 + 2 * 5 + 6
+=> 1 + 10 + 6
+=> 11 + 6
+=> 17
+
+
+Q2) 2^2^3
+ => 2^8
+ => 256
 */
