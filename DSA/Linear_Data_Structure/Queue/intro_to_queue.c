b4c8b9e1c7983a81edd072a6e22af388a6a27667
Anonymous <bkdhaniswar7991@gmail.com>
2025-11-18 15:56:16 +0100

    Introduction to queue data structure

---
+++ DSA/Linear_Data_Structure/Queue/intro_to_queue.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+
+Queue:
+1. It is a linear data structure (It is also called abstract Data type)
+2. Queue is a collection or an order list works on First In First Out (FIFO) principle
+3. Queue is the structure which is going to flow the some restriction on insertion and on deletion operation
+4. Incase of Queue, insertion is performed from one end and that is called Rear/tail
+5. And the deletion is performed from another end and that end is known as Head/front
+6. In Queue we mostly use Rear and Front
+7. In Queue rather than Insertion() we use Enqueue() and Deletion() we use Dequeue()
+8. In Queue we have to Insert or Enqueue data from Rear(tail) and Delete or Dequeue data from Front(head)
+9. We can implement Queue using Array, Linked list, and Stack
+
+*/
