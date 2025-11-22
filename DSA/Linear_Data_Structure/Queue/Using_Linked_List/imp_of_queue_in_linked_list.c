cfff19c168023c3f2c4283b5ebc91f37a1170d0f
Anonymous <bkdhaniswar7991@gmail.com>
2025-11-22 12:43:50 +0100

    Implemented enqueue logic in the code

---
+++ DSA/Linear_Data_Structure/Queue/Using_Linked_List/imp_of_queue_in_linked_list.c
@@ -0,0 +1,97 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+/*
+Implementation of queue using linked list
+*/
+
+struct queue {
+    int data;
+    struct queue *next;
+};
+
+
+
+void enqueue(struct queue **front, struct queue **rear){
+
+    struct queue  *newnode=NULL;
+    char choice[30];
+
+    newnode = (struct queue*)malloc(sizeof(struct queue));
+
+    if(newnode==NULL){
+        printf("Memory allocation failed!\n");
+    }
+
+    printf("Enqueue data in queue: \n");
+    scanf("%d", &newnode->data);
+    newnode -> next=NULL;
+
+    if(*front==NULL && (*rear)==NULL){
+        *front=*rear=newnode;
+    }
+
+    else{
+        (*rear)->next = newnode;
+        *rear = newnode;
+    }
+
+    }
+
+
+
+
+
+
+void to_free(struct queue *front){
+
+    struct queue *temp;
+
+    while(front !=NULL){
+        temp = front;
+        front = front->next;
+        free(temp);
+    }
+
+}
+
+
+
+
+
+
+int main() {
+    struct queue *front = NULL;
+    struct queue *rear = NULL;
+    int choice;
+
+    do
+    {
+        printf("Enter the choice(1: enqueue(), 2:dequeue(), 3:display(), 4:peek())\n");
+        scanf("%d", &choice);
+
+        switch (choice){
+        case 1:
+            enqueue(&front, &rear);
+            break;
+        // case 2:
+        //     dequeue(&front, &rear);
+        //     break;
+        // case 3:
+        //     display(front);
+        //     break;
+        // case 4:
+        //     peek(front);
+        //     break;
+
+        default:
+            printf("Pleae enter the valid choice\n");
+        }
+
+    } while (choice != 0);
+
+    to_free(front);
+
+}
+
