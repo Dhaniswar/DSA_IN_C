4130f70e9915eba38a77d34906db71ee2d3c0170
Anonymous <bkdhaniswar7991@gmail.com>
2025-11-23 13:09:16 +0100

    Implemented the dequeue function

--- DSA/Linear_Data_Structure/Queue/Using_Array/imp_of_circular_queue.c
+++ DSA/Linear_Data_Structure/Queue/Using_Array/imp_of_circular_queue.c
@@ -40,3 +40,38 @@ void enqueue()
     }
 }
 
+void dequeue()
+{
+
+    if (front== -1 && rear==-1)
+    {
+        printf("Underflow condition occurs!\n");
+    }
+
+    else if (front == rear)
+    {
+        printf("the dequeue element is %d\n", queue[front]);
+        front = rear = -1;
+    }
+
+    else
+    {
+        printf("the dequeue element is %d\n", queue[front]);
+        front = (front+1)%N;
+    }
+}
+
+void peek()
+{
+
+    if (front == -1 && rear == -1)
+    {
+        printf("Empty Queue!\n");
+    }
+
+    else
+    {
+        printf("element of the queue is: %d\n", queue[front]);
+    }
+}
+
